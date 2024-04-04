e214705cfb11d010f32156b625fdabe71d320556
Vinz W <vincent.wirwicki@gmail.com>
2024-04-04 18:11:41 +0200

    small update

--- src/particules/1-fbo/1-shapes/1-TorusKnot/scene/TorusKnotScene.tsx
+++ src/particules/1-fbo/1-shapes/1-TorusKnot/scene/TorusKnotScene.tsx
@@ -2,12 +2,14 @@ import { Canvas } from "@react-three/fiber";
 // import { Preload } from "@react-three/drei";
 import { Suspense } from "react";
 import TorusKnotFBO from "../particules/TorusKnotFBO";
+import { Preload } from "@react-three/drei";
 
 const TorusKnotScene = () => {
   return (
     <Suspense fallback={null}>
       <Canvas camera={{ position: [0, 0, 15] }} dpr={1.25}>
         <TorusKnotFBO />
+        <Preload />
       </Canvas>
     </Suspense>
   );
--- src/particules/1-fbo/3-model/1-bust/scene/BustScene.tsx
+++ src/particules/1-fbo/3-model/1-bust/scene/BustScene.tsx
@@ -1,16 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import BustFBO from "../particules/BustFBO";
-// import Loading from "../../../../../layout/loader/Loader";
-import { Loader } from "@react-three/drei";
+import Loading from "../../../../../layout/loader/Loader";
 
 const BustScene = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<Loading />}>
       <Canvas camera={{ position: [0, 0, 0] }} dpr={1.5}>
         <BustFBO />
       </Canvas>
-      <Loader />
     </Suspense>
   );
 };
